dde51b6ca1210afc63b7971781589889f2814cae
Umair Bashir <115666082+bpiscess99@users.noreply.github.com>
2023-05-03 11:53:37 +0500

    Update userRoutes.js
    userRoutes app changes with router

--- netflix-api/routes/userRoutes.js
+++ netflix-api/routes/userRoutes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const { addToLikeMovies, getLikedMovies, removeFromLikedMovies } = require("../controllers/userController");
 const router = express.Router()
 
-app.post("/add", addToLikeMovies)
-app.get("/liked/:email", getLikedMovies)
-app.put("/remove/", removeFromLikedMovies)
+router.post("/add", addToLikeMovies)
+router.get("/liked/:email", getLikedMovies)
+router.put("/remove/", removeFromLikedMovies)
 
 
 module.exports = router;
